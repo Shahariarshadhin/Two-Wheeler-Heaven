00562cfbc6228b71264306da71a9b1fd6c34b4b0
shahriarshadhin <shadhin19shahriar@gmail.com>
2022-03-26 00:57:17 +0600

    added fakedata

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
       <h1>Two Wheeler Heaven</h1>
+      <h4>Choose Your Best One!</h4>
+
+      <Shop></Shop>
     </div>
   );
 }
